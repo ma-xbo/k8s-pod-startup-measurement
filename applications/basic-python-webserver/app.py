c3d855359c6fb5c8d989a22950ba91eec4f748a2
Maximilian Bossert <max.bossert@outlook.de>
2021-11-28 23:57:23 +0100

    add application

---
+++ applications/basic-python-webserver/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Webserver'
+
+@app.route('/cakes')
+def cakes():
+    return 'Yummy cakes!'
